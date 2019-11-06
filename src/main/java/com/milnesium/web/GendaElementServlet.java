fb135adf2c5f06a0a74ddf7dfbf6bfb8304ca025
Florin Jurchis <jurchis@gmail.com>
2019-11-06 23:22:07 +0200

    Updated the comments as per the discussion had Monday with Flaviu: doDelete will delete one or as many items received from the request, one by one so only a function that delete one id is sufficient now. Updating in future commint also the GET method to take not only all items but if possible filtered by a search string

--- src/main/java/com/milnesium/web/GendaElementServlet.java
+++ src/main/java/com/milnesium/web/GendaElementServlet.java
@@ -49,9 +49,6 @@ public class GendaElementServlet extends HttpServlet {
         }
     }
 
-    //Trying to do doDelete but there is already a doDelete with same params as in the case where I want to delete
-    //more than one. Instead of String id should I create a list of ids?
-
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String id = req.getParameter("id");
@@ -67,11 +64,6 @@ public class GendaElementServlet extends HttpServlet {
         }
     }
 
-    //how I will update name or surname? Can I do it in the same doPut? Should I create an if else
-    //if from Postman there is surname mentioned in JSON then go with the surname else go with something else
-    //should I check somehow if name of Var is mentioned then go with a specific part of if else?
-
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try {
@@ -86,8 +78,4 @@ public class GendaElementServlet extends HttpServlet {
             resp.sendError(500, "Internal Server Error: " + e.getMessage());
         }
     }
-
-    //How to create a doGet for getting the list of rows where name or surname are matching a specific character
-    //as getting another doGet will have the same form as above
-
 }
